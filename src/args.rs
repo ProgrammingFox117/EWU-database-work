1b6b40e7e1ec3e49ac52f0bc9fcb86cab89d05df
TheHateofTheFox <ngainer@ewu.edu>
2023-05-15 14:17:08 -0700

    masdl;jgnakj;

---
+++ src/args.rs
@@ -0,0 +1,103 @@
+use clap::{
+    Args,
+    Parser,
+    Subcommand
+};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+pub struct Args{
+    #[clap(subcommand)]
+    pub entity_type: EntityType,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum EntityType {
+    ///Create, update, delete, show users
+    User(UserCommand),
+    ///Create, update, delete
+    Sprint(SprintCommand),
+}
+
+#[derive(Debug, Args)]
+pub struct UserCommand {
+    #[clap(subcommand)]
+    pub command: UserSubcommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum UserSubcommand {
+    /// Create a new user
+    Create(CreateUser),
+
+    /// Update an existing user
+    Update(UpdateUser),
+
+    /// Delete a user
+    Delete(DeleteEntity),
+
+    /// Show all users
+    Show,
+}
+
+#[derive(Debug, Args)]
+pub struct CreateUser {
+    /// The name of the user
+    pub name: String,
+
+    /// The email of the user
+    pub email: String,
+    ///TODO more User info below
+}
+#[derive(Debug, Args)]
+pub struct UpdateUser {
+    /// The id of the user to update
+    pub id: i32,
+
+    /// The name of the user
+    pub name: String,
+
+    /// The email of the user
+    pub email: String,
+    ///TODO more User info below
+}
+
+#[derive(Debug, Args)]
+pub struct DeleteEntity {
+    /// The id of the entity to delete
+    pub id: i32,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum SprintSubcommand {
+    /// Create a new sprint
+    Create(CreateSprint),
+
+    /// Update an existing sprint
+    Update(UpdateSprint),
+
+    /// Delete a the sprint
+    Delete(DeleteEntity),
+
+    /// Show all Sprints
+    Show,
+}
+
+#[derive(Debug, Args)]
+pub struct CreateSprint {
+    /// The title of the video to create
+    pub SprintNum: i8,
+
+    /// The description of the video to create
+    pub sprint_date: String,
+}
+
+#[derive(Debug, Args)]
+pub struct UpdateSprint {
+    /// The id of the video to update
+    pub sprintNum: i8,
+
+    /// The title of the video
+    pub sprint_date: String,
+
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+mod schema;
+mod models;
+mod db;
+mod args;
+mod ops;
+
+use ops::sprint_ops::*;
+
+
+
+fn main() {}
